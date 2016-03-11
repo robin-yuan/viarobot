984f0cadb4bdd1557a60a78ec8b5720173f2b2c6
Gergely Imreh <imrehg@gmail.com>
2016-03-11 15:07:06 +0800

    add block with content

--- robot.js
+++ robot.js
@@ -8,6 +8,24 @@
         return {status: 2, msg: 'Ready'};
     };
 
+
+    function httpGetAsync(theUrl, callback) {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() {
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                callback(xmlHttp.responseText);
+        };
+        xmlHttp.open("GET", theUrl, true); // true for asynchronous
+        xmlHttp.send(null);
+    }
+
+    ext.my_first_block = function() {
+        httpGetAsync("http://192.168.0.1:8080/?action=command&command="+"move_backward");
+        setTimeout(function() {
+            httpGetAsync("http://192.168.0.1:8080/?action=command&command="+"stop");
+        }, 1000);
+    };
+
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
