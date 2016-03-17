14e7e7a36bbbb0fc93faf7cc959dde2e2871c497
Gergely Imreh <imrehg@gmail.com>
2016-03-17 14:24:30 +0800

    refactoring the code

--- robot.js
+++ robot.js
@@ -1,63 +1,67 @@
-(function(ext) {
+/*jslint browser: true*/
+/*global $, ScratchExtensions */
+"use strict";
+(function (ext) {
     // Default step duration: 0.5s
-    var STEP_DURATION = 0.5;
+    var STEP_DURATION = 0.5,
+        API_BASE_URL = "http://192.168.0.1:8080/?action=command&command=",
+
+        // Asynchronous HTTP Get Request
+        httpGetAsync = function (theUrl, callback) {
+            var xmlHttp = new XMLHttpRequest();
+            xmlHttp.onreadystatechange = function () {
+                if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+                    callback(xmlHttp.responseText);
+                }
+            };
+            xmlHttp.open("GET", theUrl, true); // true for asynchronous
+            xmlHttp.send(null);
+        },
+
+        sendMove = function (command, duration) {
+            httpGetAsync(API_BASE_URL + command);
+            if (duration) {
+                setTimeout(function () {
+                    httpGetAsync(API_BASE_URL + "stop");
+                }, duration * 1000);
+            }
+        },
+
+        // Block and block menu descriptions
+        descriptor = {
+            blocks: [
+                [' ', 'move forward for %n seconds', 'move_forward', STEP_DURATION],
+                [' ', 'move backward for %n seconds', 'move_backward', STEP_DURATION],
+                [' ', 'turn left for %n seconds', 'turn_left', STEP_DURATION],
+                [' ', 'turn right for %n seconds', 'turn_right', STEP_DURATION]
+            ]
+        };
+
+    ext.move_forward = function (duration) {
+        sendMove("move_backward", duration);
+    };
+
+    ext.move_backward = function (duration) {
+        sendMove("move_forward", duration);
+    };
+
+    ext.turn_right = function (duration) {
+        sendMove("turn_right", duration);
+    };
+
+    ext.turn_left = function (duration) {
+        sendMove("turn_left", duration);
+    };
 
     // Cleanup function when the extension is unloaded
-    ext._shutdown = function() {};
+    ext._shutdown = function () {
+        sendMove("stop");
+    };
 
     // Status reporting code
     // Use this to report missing hardware, plugin or unsupported browser
-    ext._getStatus = function() {
+    ext._getStatus = function () {
         return {status: 2, msg: 'Ready'};
-    };
-
-    // Asynchronous HTTP Get Request
-    function httpGetAsync(theUrl, callback) {
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.onreadystatechange = function() {
-            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-                callback(xmlHttp.responseText);
-        };
-        xmlHttp.open("GET", theUrl, true); // true for asynchronous
-        xmlHttp.send(null);
-    }
-
-    ext.move_forward = function(duration) {
-        httpGetAsync("http://192.168.0.1:8080/?action=command&command="+"move_backward");
-        setTimeout(function() {
-            httpGetAsync("http://192.168.0.1:8080/?action=command&command="+"stop");
-        }, duration*1000);
-    };
-
-    ext.move_backward = function(duration) {
-        httpGetAsync("http://192.168.0.1:8080/?action=command&command="+"move_forward");
-        setTimeout(function() {
-            httpGetAsync("http://192.168.0.1:8080/?action=command&command="+"stop");
-        }, duration*1000);
-    };
-
-    ext.turn_right = function(duration) {
-        httpGetAsync("http://192.168.0.1:8080/?action=command&command="+"turn_right");
-        setTimeout(function() {
-            httpGetAsync("http://192.168.0.1:8080/?action=command&command="+"stop");
-        }, duration*1000);
-    };
-
-    ext.turn_left = function(duration) {
-        httpGetAsync("http://192.168.0.1:8080/?action=command&command="+"turn_left");
-        setTimeout(function() {
-            httpGetAsync("http://192.168.0.1:8080/?action=command&command="+"stop");
-        }, duration*1000);
-    };
-
-    // Block and block menu descriptions
-    var descriptor = {
-        blocks: [
-            [' ', 'move forward for %n seconds', 'move_forward', STEP_DURATION],
-            [' ', 'move backward for %n seconds', 'move_backward', STEP_DURATION],
-            [' ', 'turn left for %n seconds', 'turn_left', STEP_DURATION],
-            [' ', 'turn right for %n seconds', 'turn_right', STEP_DURATION]
-        ]
     };
 
     // Register the extension
